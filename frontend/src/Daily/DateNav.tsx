8d57543c61355e84cd181f93d42aa4b34dd7e2b3
junsushin-dev <junsushin4546@gmail.com>
2020-12-27 20:38:48 +0900

    feat: add date change on DateNav button click
    - increments or decrements displayed date by 1 day

--- frontend/src/Daily/DateNav.tsx
+++ frontend/src/Daily/DateNav.tsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import { IconButton } from '@material-ui/core';
 import { ChevronLeft, ChevronRight } from '@material-ui/icons';
 
+const DAY_MILLISEC = 1000 * 60 * 60 * 24;
+
 function DateNav() {
   const [date, setDate] = useState(new Date());
 
+  const handleDateChange = (dayOffset: number) => {
+    setDate(prevDate => new Date(prevDate.getTime() + dayOffset * DAY_MILLISEC));
+  }
+
   return (
     <div>
-      <IconButton>
+      <IconButton onClick={() => handleDateChange(-1)}>
         <ChevronLeft />
       </IconButton>
       {date.toLocaleDateString()}
-      <IconButton>
+      <IconButton onClick={() =>handleDateChange(+1)}>
         <ChevronRight />
       </IconButton>
     </div>
