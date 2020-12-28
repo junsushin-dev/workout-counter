a28f081f1d08037a7a2d089a07d7f95447e686bc
junsushin-dev <junsushin4546@gmail.com>
2020-12-28 23:19:55 +0900

    feat: add spring boot project template for backend
    - DB connection settings
    - Entry point
    - Gradle settings

---
+++ backend/src/test/java/com/example/workoutcounter/WorkoutCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workoutcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
