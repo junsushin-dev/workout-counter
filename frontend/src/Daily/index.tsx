176b40225f26dddd2b36d39b4080ca5adcd863f7
junsushin-dev <junsushin4546@gmail.com>
2021-01-11 21:55:34 +0900

    feat: add fetch api call to daily workouts page
    - workouts are now served via an API rather than static data

--- frontend/src/Daily/index.tsx
+++ frontend/src/Daily/index.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Grid, Typography } from '@material-ui/core';
 import DateNav from './DateNav';
 import Workout from './Workout';
 
+interface WorkoutResponse {
+  id: number;
+  exercise: {
+    id: number;
+    name: string;
+    count: number;
+  }
+  done: number;
+  date: string;
+}
+
 function Daily() {
   const titleText = "Today's Workout";
   const countOffset = 5;
+  const [workouts, setWorkouts] = useState<WorkoutResponse[]>([]);
 
-  const workouts = [
-    {
-      name: 'PullUps',
-      targetCount: 50,
-    },
-    {
-      name: 'PushUps',
-      targetCount: 100,
-    },
-  ]
+  useEffect(() => {
+    (async (): Promise<void> => {
+      const [dateString] = new Date().toISOString().split('T');
+      const res = await fetch(`/api/workouts?date=${dateString}`, {
+        headers: {
+          "Accept": 'application/json',
+        },
+      });
+      const data = await res.json();
+      setWorkouts(data.workouts);
+    })();
+  }, []);
 
   return (
     <Box display='flex' flexDirection='column'>
@@ -24,9 +38,9 @@ function Daily() {
       <Typography>{titleText}</Typography>
       <Box p={2}>
         <Grid container spacing={2}>
-          {workouts.map(({ name, targetCount }) => (
-            <Grid key={name} item xs={12}>
-              <Workout name={name} targetCount={targetCount} offset={countOffset}/>
+          {workouts.map(({ exercise }) => (
+            <Grid key={exercise.name} item xs={12}>
+              <Workout name={exercise.name} targetCount={exercise.count} offset={countOffset}/>
             </Grid>
           ))}
         </Grid>
