5c4eb01b6c38d85596efc3c2c188ce7edf6822b5
junsushin-dev <junsushin4546@gmail.com>
2021-01-12 18:00:02 +0900

    feat: add done count path API for workout
    - User can update workout done count via API call

--- backend/src/main/java/com/example/workoutcounter/WorkoutController.java
+++ backend/src/main/java/com/example/workoutcounter/WorkoutController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,5 +44,14 @@ public class WorkoutController {
         workouts = routines.stream().map(routine -> new Workout(routine.getExercise(), date)).collect(Collectors.toList());
         workoutRepository.saveAll(workouts);
         return new GetWorkoutsResponse(workouts);
+    }
+
+    @PatchMapping
+    public @ResponseBody Workout patchWorkout(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestParam String name, @RequestParam Integer done) {
+        List<Workout> workouts = workoutRepository.findByDateAndExerciseName(date, name);
+        Workout workout = workouts.get(0);
+        workout.setDone(done);
+        workoutRepository.save(workout);
+        return workout;
     }
 }
--- backend/src/main/java/com/example/workoutcounter/WorkoutRepository.java
+++ backend/src/main/java/com/example/workoutcounter/WorkoutRepository.java
@@ -1,5 +1,6 @@
 package com.example.workoutcounter;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
@@ -7,4 +8,6 @@ import java.util.List;
 
 public interface WorkoutRepository extends  CrudRepository<Workout, Integer> {
     List<Workout> findByDate(LocalDate date);
+    @Query("SELECT w,e FROM Workout w JOIN w.exercise e WHERE w.date = ?1 AND e.name = ?2")
+    List<Workout> findByDateAndExerciseName(LocalDate date, String name);
 }
