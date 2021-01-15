e824aa688c5ed0ab0ecef3c361374cdb132343c8
junsushin-dev <junsushin4546@gmail.com>
2021-01-15 18:05:31 +0900

    feat: add timezone env to getDateString() test
    - Timezone set to Asia/Seoul respecting main region
    - Intended for use in CI server

--- frontend/src/Daily/services.test.ts
+++ frontend/src/Daily/services.test.ts
@@ -2,6 +2,10 @@ import { getDateString } from '../../src/Daily/services';
 
 describe('test getDateString function', () => {
 
+  describe('timezone should be set to Asia/Seoul(UTC+09:00)', () => {
+    expect(new Date().getTimezoneOffset()).toBe(-540);
+  });
+
   describe('when given KST(UTC+09:00) date object at 2021-01-14 23:59', () => {
     const dateString = 'Thu Jan 14 2021 23:59:00 GMT+0900 (대한민국 표준시)';
     const date = new Date(dateString);
