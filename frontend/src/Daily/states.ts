2701273e75f382221a51b5040b7a99d9c44361d0
junsushin-dev <junsushin4546@gmail.com>
2021-01-31 17:44:29 +0900

    feat: add types for atoms and selectors
    - Added for clarification

--- frontend/src/Daily/states.ts
+++ frontend/src/Daily/states.ts
@@ -2,18 +2,18 @@ import { atom, selector } from 'recoil';
 import { IWorkout } from './types';
 import { getWorkouts } from './services';
 
-const dateState = atom({
+const dateState = atom<Date>({
   key: 'dateState',
   default: new Date(),
 });
 
-const getWorkoutsQuery = selector({
+const getWorkoutsQuery = selector<IWorkout[]>({
   key: 'currentWorkouts',
   get: async ({get}) => {
     const date = get(dateState);
     const workouts = await getWorkouts(date);
     return workouts;
   },
-})
+});
 
 export { dateState, getWorkoutsQuery };
