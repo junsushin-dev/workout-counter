aed1c6a9b338ff82bfea32751e4e3535c78a0cbd
junsushin-dev <junsushin4546@gmail.com>
2021-04-02 09:33:29 +0900

    feat: Add workout module to root module

--- workout-counter-backend/src/app.module.ts
+++ workout-counter-backend/src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExercisesModule } from './exercises/exercises.module';
 import { RoutinesModule } from './routines/routines.module';
+import { WorkoutsModule } from './workouts/workouts.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({}), ExercisesModule, RoutinesModule],
+  imports: [
+    TypeOrmModule.forRoot({}),
+    ExercisesModule,
+    RoutinesModule,
+    WorkoutsModule,
+  ],
 })
 export class AppModule {}
