60c1eb45ae4515dcabd76cc25af4cb19ac3a1077
junsushin-dev <junsushin4546@gmail.com>
2021-04-05 23:27:58 +0900

    feat: Add joined exercise data to get routines API

--- workout-counter-backend/src/routines/routines.service.ts
+++ workout-counter-backend/src/routines/routines.service.ts
@@ -23,7 +23,7 @@ export class RoutinesService {
   }
 
   async findAll(): Promise<Routine[]> {
-    return this.routineRepository.find();
+    return this.routineRepository.find({ relations: ['exercises'] });
   }
 
   async findOne(id: string): Promise<Routine> {
