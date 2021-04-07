ca738313f36a957c8503e5f3f8d08d6f675828f2
junsushin-dev <junsushin4546@gmail.com>
2021-04-07 23:15:11 +0900

    feat: Apply react-query for workouts API

--- frontend/src/Daily/DailyContent.tsx
+++ frontend/src/Daily/DailyContent.tsx
@@ -1,14 +1,27 @@
+import { useQuery } from "react-query";
 import { useRecoilValue } from "recoil";
 import WorkoutList from "./components/WorkoutList";
 import SelectRoutine from './SelectRoutine/RoutineList';
-import { getWorkoutsQuery } from "./states";
+import { getWorkouts, getDateString } from "./services";
+import { dateState, getWorkoutsQuery } from "./states";
+import { IWorkout } from "./types";
 
 export function DailyContent() {
-  const workouts = useRecoilValue(getWorkoutsQuery);
+  // const workouts = useRecoilValue(getWorkoutsQuery);
+  const date = useRecoilValue(dateState);
+  const workoutQuery = useQuery<IWorkout[], Error>(`workouts/${getDateString(date)}`, () => getWorkouts(date));
 
+  if (workoutQuery.isIdle || workoutQuery.isLoading) {
+    return <span>loading...</span>;
+  }
+
+  if (workoutQuery.isError) {
+    return <span>{workoutQuery.error}</span>
+  }
+  
   return (
     <>
-      {workouts.length > 0 ? <WorkoutList /> : <SelectRoutine />}
+      {workoutQuery.data.length > 0 ? <WorkoutList /> : <SelectRoutine />}
     </>
   )
 }
