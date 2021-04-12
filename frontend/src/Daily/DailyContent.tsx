5bdcc6951bd3065c490bc40ca70e2d3e6831e9ea
junsushin-dev <junsushin4546@gmail.com>
2021-04-12 21:32:24 +0900

    refactor: Remove unused variable imports and comments

--- frontend/src/Daily/DailyContent.tsx
+++ frontend/src/Daily/DailyContent.tsx
@@ -3,11 +3,10 @@ import { useRecoilValue } from "recoil";
 import WorkoutList from "./components/WorkoutList";
 import SelectRoutine from './SelectRoutine/RoutineList';
 import { getWorkouts, getDateString } from "./services";
-import { dateState, getWorkoutsQuery } from "./states";
+import { dateState } from "./states";
 import { IWorkout } from "./types";
 
 export function DailyContent() {
-  // const workouts = useRecoilValue(getWorkoutsQuery);
   const date = useRecoilValue(dateState);
   const workoutQuery = useQuery<IWorkout[], Error>(`workouts/${getDateString(date)}`, () => getWorkouts(date));
 
