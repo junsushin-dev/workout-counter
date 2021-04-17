96851333ddcc2ddf00781d8a51b74e004e144e0c
junsushin-dev <junsushin4546@gmail.com>
2021-04-17 23:40:46 +0900

    feat: Add sentry for frontend React project

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://9677dd22556b46eea84fefe05ab98181@o476959.ingest.sentry.io/5724161",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
