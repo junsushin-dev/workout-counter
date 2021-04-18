fdf62f9bf119914528336490cd3311473a2107ad
junsushin-dev <junsushin4546@gmail.com>
2021-04-18 16:35:55 +0900

    refactor: Use configModule instead of ormconfig.json
    - TypeORMModule only resolves ormconfig.json at project root, which
    is incompatible with yarn workspaces
    - Use configModule to inject environment variables into TypeORMModule

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,10 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExercisesModule } from './exercises/exercises.module';
 import { RoutinesModule } from './routines/routines.module';
 import { WorkoutsModule } from './workouts/workouts.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({}),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        "type": "mysql",
+        "host": configService.get('DB_HOST'),
+        "port": configService.get<number>('DB_PORT'),
+        "username": configService.get('DB_USERNAME'),
+        "password": configService.get('DB_PASSWORD'),
+        "database": configService.get('DB_SCHEMA'),
+        "entities": ["dist/**/*.entity{.ts,.js}"],
+        "synchronize": true,
+      }),
+      inject: [ConfigService],
+    }),
     ExercisesModule,
     RoutinesModule,
     WorkoutsModule,
