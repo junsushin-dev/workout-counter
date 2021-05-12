6c629f83638c54385d8aee5d9a373e0c600d0004
junsushin-dev <junsushin4546@gmail.com>
2021-05-12 23:48:43 +0900

    refactor: Remove legacy cache related code from services

--- packages/frontend/src/Daily/services.ts
+++ packages/frontend/src/Daily/services.ts
@@ -7,39 +7,16 @@ export const getDateString = (date: Date):string => new Date(date.getTime() - da
 
 export const getTodayString = ():string => getDateString(new Date());
 
-let workoutsCache = new Map();
-
-const updateWorkoutsCache = async (date: Date):Promise<void> => {
-  const dateString = getDateString(date);
-  if(workoutsCache.has(dateString)) return;
-  const workouts = await getWorkoutsByDateString(dateString);
-  if(workouts === undefined || workouts === null) {
-    return;
-  }
-  workoutsCache.set(dateString, workouts);
-}
-
 export const getWorkouts = async (date: Date):Promise<IWorkout[]> => {
   const dateString = getDateString(date);
   
-  const cachedWorkouts = workoutsCache.get(dateString);
-  if(cachedWorkouts !== undefined) return cachedWorkouts;
-
-  await updateWorkoutsCache(date);
-  updateWorkoutsCache(new Date(date.getTime() + DAY_MILLISECS));
-  updateWorkoutsCache(new Date(date.getTime() - DAY_MILLISECS));
-
-  return workoutsCache.get(dateString);
-}
-
-export const getWorkoutsByDateString = async (dateString: String): Promise<IWorkout[] | null> => {
   const res = await fetch(`/api/workouts?date=${dateString}`, {
     headers: {
       'Accept': 'application/json',
     },
   });
   if(res.status === 404) {
-    return null;
+    return [];
   }
   const workouts = await res.json();
   return workouts;
@@ -56,13 +33,7 @@ export const updateDoneCount = async (id: number, date: Date, doneCount: number)
     body,
   });
   const updatedWorkout = await res.json();
-
-  const dateString = getDateString(date);
-  const cachedWorkouts = workoutsCache.get(dateString);
-  const cachedIndex = cachedWorkouts.findIndex((workout: IWorkout) => workout.id === updatedWorkout.id);
-  const updatedWorkouts = [...cachedWorkouts.slice(0, cachedIndex), updatedWorkout, ...cachedWorkouts.slice(cachedIndex + 1)];
-  workoutsCache.set(dateString, updatedWorkouts);
-  console.log(workoutsCache);
+  return updatedWorkout;
 }
 
 export const getRoutines = async ():Promise<IRoutine[]> => {
@@ -85,13 +56,11 @@ const createWorkoutsByExercises = async (date: Date, exercises: IExercise[]): Pr
     }),
   })
 
-  const workout = await res.json();
-  return workout;
+  const workouts = await res.json();
+  return workouts;
 }
 
 export const createWorkoutsByRoutine = async (date: Date, routine: IRoutine): Promise<IWorkout[]> => {
-  const dateString = getDateString(date);
   const workouts: IWorkout[] = await createWorkoutsByExercises(date, routine.exercises);
-  workoutsCache.set(dateString, workouts);
   return workouts;
 }
