116108c71c51532d2d352fba734da9376267d137
junsushin-dev <junsushin4546@gmail.com>
2021-05-14 23:35:36 +0900

    feat: Replace API with react-query in WorkoutList

--- packages/frontend/src/Daily/components/WorkoutList.tsx
+++ packages/frontend/src/Daily/components/WorkoutList.tsx
@@ -1,15 +1,27 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useRecoilValue } from 'recoil';
 
-import { getDateString } from '../services';
-import { dateState, getWorkoutsQuery } from '../states';
+import { getDateString, getWorkouts } from '../services';
+import { dateState } from '../states';
+import { IWorkout } from '../types';
 import Workout from './Workout';
 
 function WorkoutList() {
   const date = useRecoilValue(dateState);
-  const workouts = useRecoilValue(getWorkoutsQuery);
+  const workoutQuery = useQuery<IWorkout[], Error>(`workouts/${getDateString(date)}`, () => getWorkouts(date));
   
+  if (workoutQuery.isIdle || workoutQuery.isLoading) {
+    return <span>loading...</span>;
+  }
+
+  if (workoutQuery.isError) {
+    return <span>{workoutQuery.error}</span>
+  }
+
+  const workouts = workoutQuery.data;
+
   return (
     <Grid container spacing={2}>
       {workouts.map((workout) => (
