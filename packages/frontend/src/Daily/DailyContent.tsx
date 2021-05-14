ba34421df4f2713e9dc07cb5d06f71f19db57ef9
junsushin-dev <junsushin4546@gmail.com>
2021-05-14 23:52:57 +0900

    feat: Add prevDate, nextDate prefetch using react-query

--- packages/frontend/src/Daily/DailyContent.tsx
+++ packages/frontend/src/Daily/DailyContent.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { useQuery } from "react-query";
+import React, { useEffect } from 'react';
+import { useQuery, useQueryClient } from "react-query";
 import { useRecoilValue } from "recoil";
 
 import WorkoutList from "./components/WorkoutList";
 import SelectRoutine from './SelectRoutine/RoutineList';
-import { getDateString,getWorkouts } from "./services";
+import { DAY_MILLISECS, getDateString,getWorkouts } from "./services";
 import { dateState } from "./states";
 import { IWorkout } from "./types";
 
 export function DailyContent() {
   const date = useRecoilValue(dateState);
   const workoutQuery = useQuery<IWorkout[], Error>(`workouts/${getDateString(date)}`, () => getWorkouts(date));
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const prefetchWorkouts = async (date: Date) => {
+      await queryClient.prefetchQuery(`workouts/${getDateString(date)}`, () => getWorkouts(date));
+    }
+
+    const prevDate = new Date(date.getTime() - DAY_MILLISECS);
+    const nextDate = new Date(date.getTime() + DAY_MILLISECS);
+    prefetchWorkouts(prevDate);
+    prefetchWorkouts(nextDate);
+  }, [date, queryClient]);
 
   if (workoutQuery.isIdle || workoutQuery.isLoading) {
     return <span>loading...</span>;
--- packages/frontend/src/Daily/services.ts
+++ packages/frontend/src/Daily/services.ts
@@ -1,7 +1,7 @@
 import { IExercise,IRoutine, IWorkout } from './types';
 
 const MINUTE_MILLISECS = 60 * 1000;
-const DAY_MILLISECS = 24 * 60 * MINUTE_MILLISECS;
+export const DAY_MILLISECS = 24 * 60 * MINUTE_MILLISECS;
 
 export const getDateString = (date: Date):string => new Date(date.getTime() - date.getTimezoneOffset() * MINUTE_MILLISECS).toISOString().split('T')[0];
 
