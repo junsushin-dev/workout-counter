e75ec3271794582b09ff40a185af60ce310cd3df
junsushin-dev <junsushin4546@gmail.com>
2021-05-19 20:40:19 +0900

    chore: Removed unused recoil selectors

--- packages/frontend/src/Daily/states.ts
+++ packages/frontend/src/Daily/states.ts
@@ -1,28 +1,6 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 
-import { getRoutines,getWorkouts } from '../apis/workoutsAPI';
-import { IRoutine,IWorkout } from './types';
-
-const dateState = atom<Date>({
+export const dateState = atom<Date>({
   key: 'dateState',
   default: new Date(),
 });
-
-const getWorkoutsQuery = selector<IWorkout[]>({
-  key: 'currentWorkouts',
-  get: async ({get}) => {
-    const date = get(dateState);
-    const workouts = await getWorkouts(date);
-    return workouts;
-  },
-});
-
-const getRoutinesQuery = selector<IRoutine[]>({
-  key: 'Routines',
-  get: async () => {
-    const routines = await getRoutines();
-    return routines;
-  },
-});
-
-export { dateState, getRoutinesQuery,getWorkoutsQuery };
