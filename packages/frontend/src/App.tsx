0f781b970365d84bed90bda526bc7ea569e209a3
junsushin-dev <junsushin4546@gmail.com>
2021-05-23 00:20:13 +0900

    feat: Change tabs to use routing

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,53 +2,53 @@ import './App.css';
 import 'fontsource-roboto';
 
 import { AppBar, Tab, Tabs } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
-import { TabPanel } from './views/common/TabPanel';
+import { useRerender } from './hooks/useRerender';
 import Daily from './views/Daily';
 import { ExercisesTab } from './views/Exercises';
 
 const queryClient = new QueryClient();
 
 function App() {
-
-  const [currTab, setCurrTab] = useState(0);
-
-  const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
-    setCurrTab(newValue);
-  }
+  const rerender = useRerender();
 
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <div className="App">
-          <AppBar position="static">
-            <Tabs value={currTab} onChange={handleChange}>
-              <Tab label="Daily" />
-              <Tab label="Monthly" />
-              <Tab label="Exercises" />
-              <Tab label="Routines" />
-              <Tab label="Settings" />
-            </Tabs>
-          </AppBar>
-          <TabPanel index={0} value={currTab}>
-            <Daily />
-          </TabPanel>
-          <TabPanel index={1} value={currTab}>
-            Monthly
-          </TabPanel>
-          <TabPanel index={2} value={currTab}>
-            <ExercisesTab />
-          </TabPanel>
-          <TabPanel index={3} value={currTab}>
-            Routines
-          </TabPanel>
-          <TabPanel index={4} value={currTab}>
-            Settings
-          </TabPanel>
-        </div>
+        <BrowserRouter>
+          <div className="App">
+            <AppBar position="static">
+              <Tabs value={window.location.pathname} onChange={rerender}>
+                <Tab label="Daily" component={Link} to='/daily' value='/daily' />
+                <Tab label="Monthly" component={Link} to='/monthly' value='/monthly' />
+                <Tab label="Exercises" component={Link} to='/exercises' value='/exercises' />
+                <Tab label="Routines" component={Link} to='/routines' value='/routines' />
+                <Tab label="Settings" component={Link} to='/settings' value='/settings' />
+              </Tabs>
+            </AppBar>
+            <Switch>
+              <Route path='/daily'>
+                <Daily />
+              </Route>
+              <Route path='/monthly'>
+                Monthly
+              </Route>
+              <Route path='/exercises'>
+                <ExercisesTab />
+              </Route>
+              <Route path='/routines'>
+                Routines
+              </Route>
+              <Route path='/settings'>
+                Settings
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
       </RecoilRoot>
     </QueryClientProvider>
   );
