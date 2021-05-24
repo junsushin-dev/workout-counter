7e3851ee42bc0380b8b5cdd889ad98c879f0406f
junsushin-dev <junsushin4546@gmail.com>
2021-05-24 23:07:06 +0900

    chore: Disable lint error for useRerender
    count variable is just a placeholder, only setCount is used

--- packages/frontend/src/hooks/useRerender.ts
+++ packages/frontend/src/hooks/useRerender.ts
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 export function useRerender() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [count, setCount] = useState(0);
 
   return () => setCount(n => n + 1);
