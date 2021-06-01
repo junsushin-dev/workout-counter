cff8755ef48c259b0f39576a6fedfd2e5426966d
junsushin-dev <junsushin4546@gmail.com>
2021-06-01 09:22:28 +0900

    feat: Add API call func for adding exercise

--- packages/frontend/src/apis/exercisesAPI.ts
+++ packages/frontend/src/apis/exercisesAPI.ts
@@ -1,7 +1,21 @@
+import { AddExerciseDTO } from "../types";
 import { customFetch } from "../utils/customFetch";
 
 export const getExercises = async () => {
   const exercises = await customFetch('/api/exercises');
 
   return exercises;
+}
+
+export const addExercise = async (exercise: AddExerciseDTO) => {
+  const addedExercise = await customFetch('/api/exercises', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(exercise),
+  });
+
+  return addedExercise;
 }
