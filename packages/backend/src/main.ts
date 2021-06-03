69b0851fe22a40b13fafec740f482f2decfcf1d2
junsushin-dev <junsushin4546@gmail.com>
2021-06-03 23:07:37 +0900

    feat: Add sentry http integrations for backend

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import * as Sentry from '@sentry/node';
-import * as Tracing from '@sentry/tracing';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   Sentry.init({
     dsn: 'https://aabcba7e9ada4ee3827164608b3442be@o476959.ingest.sentry.io/5736519',
+    integrations: [new Sentry.Integrations.Http({ tracing: true })],
     tracesSampleRate: 1.0,
   });
-  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
