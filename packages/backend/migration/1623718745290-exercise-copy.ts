d72d16a9ddc0397412c9e7cb097f763db7e46853
junsushin-dev <junsushin4546@gmail.com>
2021-06-19 18:24:45 +0900

    feat: Preserve foreign key on migration
    Leave FK as reference data for cases when you need to track the original
    exercise that the workout copied from.

--- packages/backend/migration/1623718745290-exercise-copy.ts
+++ packages/backend/migration/1623718745290-exercise-copy.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from 'typeorm';
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class exerciseCopy1623718745290 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -11,34 +11,9 @@ export class exerciseCopy1623718745290 implements MigrationInterface {
         INNER JOIN exercise ON workout.exercise_id = exercise.id
         SET workout.exercise_name = exercise.name, workout.exercise_count = exercise.count
     `);
-    const table = await queryRunner.getTable('workout');
-    const foreignKey = table.foreignKeys.find((fk) => fk.columnNames.includes('exercise_id'));
-    await queryRunner.dropForeignKey('workout', foreignKey);
-    await queryRunner.dropColumn('workout', 'exercise_id');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumn(
-      'workout',
-      new TableColumn({
-        name: 'exercise_id',
-        type: 'int',
-      })
-    );
-    await queryRunner.query(`
-    UPDATE workout
-      INNER JOIN exercise ON workout.exercise_name = exercise.name
-      SET workout.exercise_id = exercise.id
-    `);
-    await queryRunner.createForeignKey(
-      'workout',
-      new TableForeignKey({
-        columnNames: ['exercise_id'],
-        referencedColumnNames: ['id'],
-        referencedTableName: 'exercise',
-        onDelete: 'NO ACTION',
-      })
-    );
     await queryRunner.dropColumn('workout', 'exercise_name');
     await queryRunner.dropColumn('workout', 'exercise_count');
   }
