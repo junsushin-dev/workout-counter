4e6c1edf97cfce82bd4e74dd7b8b53c533de5870
junsushin-dev <junsushin4546@gmail.com>
2021-07-06 09:44:21 +0900

    refactor: Change RoutineItem to take click handler as props

--- packages/frontend/src/views/Daily/SelectRoutine/RoutineList.tsx
+++ packages/frontend/src/views/Daily/SelectRoutine/RoutineList.tsx
@@ -1,13 +1,26 @@
 import { Box, Grid } from '@material-ui/core';
 import React from 'react';
+import { useQueryClient } from 'react-query';
+import { useRecoilValue } from 'recoil';
 
+import { createWorkoutsByRoutine } from '../../../apis/workoutsAPI';
 import { useRoutines } from '../../../hooks/useRoutines';
+import { IRoutine } from '../../../types';
+import { getDateString } from '../../../utils/getDateString';
 import CenteredProgress from '../../common/CenteredProgress';
 import { ErrorMessage } from '../../common/ErrorMessage';
+import { dateState } from '../states';
 import RoutineItem from './RoutineItem';
 
 function RoutineList() {
   const routinesQuery = useRoutines();
+  const queryClient = useQueryClient();
+  const date = useRecoilValue(dateState);
+
+  const handleClick = (routine: IRoutine) => async () => {
+    await createWorkoutsByRoutine(date, routine);
+    queryClient.invalidateQueries(`workouts/${getDateString(date)}`);
+  };
 
   if (routinesQuery.isIdle || routinesQuery.isLoading) {
     return <CenteredProgress />;
@@ -24,7 +37,7 @@ function RoutineList() {
       <Grid container direction="column" spacing={2}>
         {routines.map((routine) => (
           <Grid item key={routine.id}>
-            <RoutineItem routine={routine} />
+            <RoutineItem routine={routine} onClick={handleClick(routine)} />
           </Grid>
         ))}
       </Grid>
