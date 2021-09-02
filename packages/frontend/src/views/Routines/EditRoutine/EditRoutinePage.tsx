97efed4424f084d96ee62a3f733edbce9c804499
junsushin-dev <junsushin4546@gmail.com>
2021-09-02 23:15:21 +0900

    feat: Persist drag result inside & between columns

--- packages/frontend/src/views/Routines/EditRoutine/EditRoutinePage.tsx
+++ packages/frontend/src/views/Routines/EditRoutine/EditRoutinePage.tsx
@@ -1,11 +1,13 @@
 import { Box, Grid, Paper, styled, Typography } from '@material-ui/core';
 import { KeyboardArrowUp as KeyboardArrowUpIcon } from '@material-ui/icons';
 import React from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
+import { useMutation, useQueryClient } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 import { useExercises } from '../../../hooks/useExercises';
 import { useRoutine } from '../../../hooks/useRoutine';
+import { IExercise, IRoutine } from '../../../types';
 import CenteredProgress from '../../common/CenteredProgress';
 import { ErrorMessage } from '../../common/ErrorMessage';
 import { ExerciseItem } from './ExerciseItem';
@@ -26,6 +28,22 @@ export function EditRoutinePage() {
   const { id } = useParams<{ id: string }>();
   const routineQuery = useRoutine(parseInt(id));
   const exercisesQuery = useExercises();
+  const queryClient = useQueryClient();
+
+  // TODO: Change fetcher to API call
+  const updateRoutineExercisesMutation = useMutation((newExercises) => Promise.resolve(newExercises), {
+    onMutate: async (newExercises: IExercise[]) => {
+      const previousRoutine = queryClient.getQueryData<IRoutine>(`routines/${id}`);
+      if (previousRoutine) {
+        queryClient.setQueryData<IRoutine>(`routines/${id}`, {
+          ...previousRoutine,
+          exercises: newExercises,
+        });
+      }
+
+      return { previousRoutine };
+    },
+  });
 
   if (routineQuery.isIdle || routineQuery.isLoading || exercisesQuery.isIdle || exercisesQuery.isLoading) {
     return <CenteredProgress />;
@@ -44,8 +62,40 @@ export function EditRoutinePage() {
     routine.exercises.every((routineExercise) => routineExercise.id !== exercise.id)
   );
 
+  const handleDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) return;
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    const start = source.droppableId === 'included' ? routine.exercises : addableExercises;
+    const finish = destination.droppableId === 'included' ? routine.exercises : addableExercises;
+
+    // move item inside column
+    if (start === finish) {
+      if (source.droppableId === 'excluded') return;
+      const newExercises = [...start];
+      const moved = newExercises.find((item) => item.id.toString() === draggableId);
+      if (!moved) return;
+      newExercises.splice(source.index, 1);
+      newExercises.splice(destination.index, 0, moved);
+      updateRoutineExercisesMutation.mutate(newExercises);
+      return;
+    }
+
+    // move item between columns
+    const newStartColumn = [...start];
+    const newFinishColumn = [...finish];
+    const moved = newStartColumn.find((item) => item.id.toString() === draggableId);
+    if (!moved) return;
+    newStartColumn.splice(source.index, 1);
+    newFinishColumn.splice(destination.index, 0, moved);
+    const newExercises = source.droppableId === 'included' ? newStartColumn : newFinishColumn;
+    updateRoutineExercisesMutation.mutate(newExercises);
+  };
+
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <GappedBox>
         <Typography variant="h5">{routine.name}</Typography>
         <Droppable droppableId="included">
